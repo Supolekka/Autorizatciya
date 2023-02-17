93268aec15186858dfd1257c1e187753a1ce2611
Данил <82579479+Supolekka@users.noreply.github.com>
2023-02-17 14:57:50 +0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    alert("Версия jQuery " + jQuery.fn.jquery);
+});
+$(".dws-form").on("click", ".tab", function() {
+    // Удаляем классы active
+    $(".dws-form .tab").removeClass('active');
+ 
+    // Добавляем классы active
+    $(this).addClass('active');
+});
+$(".dws-form").find(".active").removeClass("active");
+ $(this).addClass("active");
+ $(".tab-form").eq($(this).index()).addClass("active");
